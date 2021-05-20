49bbc3dca2cf5c88b970eb207de172b8bae990e0
A284Philipi <78768392+A284Philipi@users.noreply.github.com>
2021-05-20 20:23:29 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nota;
+    cin >> nota;
+    if (nota == 0){
+        cout << "E" <<endl;
+    }else{
+        if (nota <= 35){
+            cout << "D" <<endl;
+        }else{
+            if (nota <= 60){
+                cout << "C" <<endl;
+            }else{
+                if (nota <= 85){
+                    cout << "B" <<endl;
+                }else{
+                    cout << "A" <<endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
